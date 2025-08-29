5415c1610ef65d9f007a4c5b918f1bf13d4948a7
rohit-singh-bisht <thersbisht@gmail.com>
2025-08-29 15:51:46 +0530

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,14 @@ export default function Home() {
       </SidebarProvider>
       <main className="w-full flex flex-col">
         <Header />
-        <div></div>
-        <ChatMessageInput />
+        <div className="relative flex basis-auto flex-col grow overflow-hidden">
+          <div className="relative h-full"></div>
+        </div>
+        <div className="text-base mx-auto [--thread-content-margin:--spacing(4)] @[37rem]:[--thread-content-margin:--spacing(6)] @[72rem]:[--thread-content-margin:--spacing(16)] px-(--thread-content-margin)">
+          <div className="[--thread-content-max-width:32rem] @[34rem]:[--thread-content-max-width:40rem] @[64rem]:[--thread-content-max-width:48rem] mx-auto max-w-(--thread-content-max-width) flex-1">
+            <ChatMessageInput />
+          </div>
+        </div>
       </main>
     </div>
   );
--- src/components/common/chat-message-input.tsx
+++ src/components/common/chat-message-input.tsx
@@ -1,11 +1,14 @@
 export default function ChatMessageInput() {
   return (
-    <div className="p-2 border-t touch:p-2.5">
-      <textarea
+    <div className="p-2 border-t touch:p-2.5 w-full flex items-center gap-2 bg-gray-50 dark:bg-gray-900">
+      {/* <textarea
         className="w-full resize-none rounded-md border border-gray-300 bg-white p-2 text-sm shadow-sm placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder:text-gray-500 sm:text-sm"
         rows={1}
         placeholder="Type your message..."
-      ></textarea>
+      ></textarea> */}
+      <div className="max-w-70">
+        <div contentEditable="true" className="w-full"></div>
+      </div>
     </div>
   );
 }
